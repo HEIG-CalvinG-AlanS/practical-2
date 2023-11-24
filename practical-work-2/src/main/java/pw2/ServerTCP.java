a3ab7571987e946ca60441f6494a873cf669f136
AlanSottile <alan.sottile@heig-vd.ch>
2023-11-24 13:14:22 +0100

    adding the list of users

--- practical-work-2/src/main/java/pw2/ServerTCP.java
+++ practical-work-2/src/main/java/pw2/ServerTCP.java
@@ -4,15 +4,29 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List; // peut etre maven dependency????
 
 public class ServerTCP {
     private static final int PORT = 1234;
     private static final int SERVER_ID = (int) (Math.random() * 1000000);
     public static String FILE_PATH = "pw2/history.txt"; //la mettre en const
+    public static int CHATROOM_SIZE = 10;
+    private static List<ClientHandler> onlineUsers;
+
+    private static synchronized void addClientHandler(ClientHandler clientHandler) {
+        if (onlineUsers.size() < CHATROOM_SIZE) {
+            onlineUsers.add(clientHandler);
+        } else {
+            System.out.println("La chatroom est pleine. Impossible d'ajouter de nouveaux utilisateurs.");
+            // Vous pouvez ajouter une logique supplémentaire ici si nécessaire
+        }
+    }
 
     public static void main(String[] args) {
 
         try (ServerSocket serverSocket = new ServerSocket(PORT);) {
+
             System.out.println(
                     "[Server " + SERVER_ID + "] starting with id " + SERVER_ID
             );
@@ -20,15 +34,20 @@ public class ServerTCP {
                     "[Server " + SERVER_ID + "] listening on port " + PORT
             );
 
+            onlineUsers = new ArrayList<>();
+
             while (true) {
                 Socket clientSocket = serverSocket.accept();
-                Thread clientThread = new Thread(new ClientHandler(clientSocket));
+                ClientHandler clientHandler = new ClientHandler(clientSocket);
+                addClientHandler(clientHandler);
+                Thread clientThread = new Thread(clientHandler);
                 clientThread.start();
             }
         } catch (IOException e) {
             System.out.println("[Server " + SERVER_ID + "] exception: " + e);
         }
     }
+
 
     static class ClientHandler implements Runnable {
 
@@ -54,6 +73,9 @@ public class ServerTCP {
             ) {
                 System.out.println(
                         "[Server " + SERVER_ID + "] new client connected from " + socket.getInetAddress().getHostAddress() + ":" + socket.getPort()
+                );
+                System.out.println(
+                        "[Server " + SERVER_ID + "] number of online clients :" + onlineUsers.size()
                 );
 
 
@@ -81,12 +103,10 @@ public class ServerTCP {
                 out.write(lastLine + "\n"); // sert a envoyer lastline au client
                 out.flush();
 
-
-
                 // Write the user input into the history file
                 String userInput = in.readLine();
 
-                try (BufferedWriter writer = new BufferedWriter(new FileWriter("D:\\S3\\DAI\\practical-2\\practical-work-2\\src\\main\\java\\pw2\\history.txt", true))) {
+                try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_PATH, true))) {
                     writer.write(userInput + "\n"); // systems differents, pas \n
                 } catch (IOException e) {
                     e.printStackTrace();
@@ -95,7 +115,6 @@ public class ServerTCP {
                 System.out.println(
                         "[Server " + SERVER_ID + "] received textual data from client: " + userInput
                 );
-
 
                 System.out.println("[Server " + SERVER_ID + "] closing connection");
             } catch (IOException e) {
