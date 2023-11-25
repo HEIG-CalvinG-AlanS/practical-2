613eee40e2e14a5d325aec76187d7dc90a9272e1
CalvinGraf1 <calvin.graf@heig-vd.ch>
2023-11-25 01:36:34 +0100

    Add reading of history file by user

--- practical-work-2/src/main/java/pw2/ClientTCP.java
+++ practical-work-2/src/main/java/pw2/ClientTCP.java
@@ -10,7 +10,6 @@ class ClientTCP {
     private static final String HOST = "localhost";
     private static final int PORT = 1234;
     private static final int CLIENT_ID = 0;
-    private static final String TEXTUAL_DATA = "[#" + CLIENT_ID + "] Hello";
 
     public static void main(String args[]) {
         System.out.println("Connecting to the chat room via " + HOST + ":" + PORT);
@@ -29,9 +28,6 @@ class ClientTCP {
 
             System.out.println("Successful connection");
             System.out.println("Your personal ID is : " + CLIENT_ID);
-
-            out.write(TEXTUAL_DATA + "\n");
-            out.flush();
 
             User user = new User(out, CLIENT_ID);
 
--- practical-work-2/src/main/java/pw2/User.java
+++ practical-work-2/src/main/java/pw2/User.java
@@ -1,33 +1,67 @@
 package pw2;
 
-import java.io.BufferedWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 
 public class User implements Runnable {
+    public final static String FILE_PATH = "pw2/history.txt";
     private String username;
     private final Scanner message;
     private final int ID;
+    BufferedReader in;
     BufferedWriter out;
+    private boolean isReading;
 
     User(BufferedWriter out, int id) {
         this.out = out;
         this.ID = id;
+        try {
+            this.in = new BufferedReader(new FileReader(FILE_PATH));
+        }
+        catch (FileNotFoundException e) {
+            throw new RuntimeException("[#" + ID + "] Error opening history file : " + e);
+        }
 
+        isReading = true;
         message = new Scanner(System.in);
         username = "";
         changeUsername();
-        System.out.println("\nWelcome on the chat room " + username + "!\n");
-        System.out.println("To see all available command, type: /help\n");
+        System.out.println("\nWelcome on the chat room " + username + " !\n");
+        System.out.println("To see all available command, type : /help\n");
         run();
     }
 
-    public void run() {
-        String msg = "";
-        while(!msg.equals("/quit")) {
-            System.out.print("Your message: ");
-            msg = message.nextLine();
+    public void readFile() {
+        String userInput = "";
+        try {
+            while ((in.readLine()) != null) { } // Passez toutes les lignes déjà présente
+                while (isReading) {
+                    userInput = in.readLine();
+                    if (userInput != null) afficherNouveauMessage(userInput);
+                }
+        }
+        catch (IOException e) {
+            throw new RuntimeException("[#" + ID + "] Error reading history file : " + e);
+        }
+    }
 
+    private void afficherNouveauMessage(String message) {
+        // Déplacer le curseur vers le haut | Les deux lignes ci-dessous ont été réalisées par ChatGPT
+        System.out.print("\033[2K"); // Efface la ligne actuelle
+        System.out.print("\r"); // Place le curseur au début de la ligne
+
+        System.out.println(message);
+        System.out.print("> ");
+    }
+
+
+    public void run() {
+        Thread readFile = new Thread(this::readFile);
+        readFile.start();
+        String msg = "";
+        System.out.print("> ");
+        while(!msg.equals("/quit")) {
+            msg = message.nextLine();
             switch (msg) {
                 case "/username":
                     changeUsername();
@@ -42,16 +76,24 @@ public class User implements Runnable {
                     System.out.println("\n\t/online : See all connected people");
                     System.out.println("\n\t/quit : Quit the chat room\n");
                     break;
+                case "":
+                    System.out.print("\033[1A"); // Déplace le curseur vers le haut d'une ligne
+                    System.out.print("\033[2K"); // Efface la ligne
+                    System.out.print("> ");
+                    break;
                 default:
                     try {
-                       // System.out.println("Message : " + msg);
+                        System.out.print("\033[1A"); // Déplace le curseur vers le haut d'une ligne
+                        System.out.print("\033[2K"); // Efface la ligne
+
                         out.write("[#" + ID + "] " + msg + "\n");
                         out.flush();
                     } catch (IOException e) {
-                        throw new RuntimeException(e);
+                        throw new RuntimeException("[#" + ID + "] Error writing to history file" + e);
                     }
             }
         }
+        isReading = false;
     }
 
     private void changeUsername() {
@@ -60,5 +102,4 @@ public class User implements Runnable {
             this.username = message.nextLine();
         } while(username.equals(""));
     }
-
 }
